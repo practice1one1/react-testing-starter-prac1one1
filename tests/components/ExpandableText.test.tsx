0fde53350ca6c3c9807500e5d557c811675e5a66
practice1one1 <practice1one1@gmail.com>
2026-03-13 11:57:27 +0000

    test: modify tests and their descriptions / names to be specific with 255 characters

--- tests/components/ExpandableText.test.tsx
+++ tests/components/ExpandableText.test.tsx
@@ -3,27 +3,26 @@ import ExpandableText from "../../src/components/ExpandableText";
 import userEvent from "@testing-library/user-event";
 
 describe("ExpandableText", () => {
-  it("should render short text without the show more button", () => {
+  it("should render full text if less than 255 characters", () => {
     const text = "short text";
 
     render(<ExpandableText text={text} />);
 
     expect(screen.getByText(text)).toBeInTheDocument();
     expect(
-      screen.queryByRole("button", { name: /\b(show less|show more)\b/i }),
+      screen.queryByRole("button", { name: /\b(show less|show more)\b/i })
     ).not.toBeInTheDocument();
   });
 
-  it("should render very long text with ellipsis at end and the show more button", () => {
+  it("should render truncated text and the show more button if longer than 255 characters", () => {
     const text =
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit ipsa omnis sint doloribus quam velit, odit natus, eum repellendus molestias vero atque exercitationem doloremque! Praesentium cumque dolorum et eligendi ab illo, rerum vitae nam corporis porro quod cum saepe nisi illum dolor aliquid animi quos inventore aliquam esse neque. Mollitia modi expedita ipsam quasi atque nemo assumenda laboriosam minima rerum dolore maiores sed praesentium quo recusandae ipsa dolorem, voluptatem at vero sapiente illo harum. Iure ipsam dignissimos architecto voluptatem, dolorem dolorum enim sapiente assumenda neque quae ipsum a quam delectus vitae non fuga dicta tempore libero eveniet quis voluptatibus? Perferendis";
 
     render(<ExpandableText text={text} />);
 
-    expect(screen.getByRole("article")).toHaveTextContent(/\.\.\.$/);
-    const button = screen.getByRole("button");
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent(/show more/i);
+    const expectedText = text.substring(0, 255) + "...";
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+    expect(screen.getByRole("button")).toHaveTextContent(/more/i);
   });
 
   it("should expand text, hide ellipsis and change button text when show more button is cliked", async () => {
